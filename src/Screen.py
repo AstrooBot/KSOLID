7ba7f304f8c8ae9854112bd6ef3f57d0f4139e96
AstrooBot <129632296+AstrooBot@users.noreply.github.com>
2024-04-09 20:03:02 -0500

    Add files via upload

--- src/Screen.py
+++ src/Screen.py
@@ -9,26 +9,48 @@ screen_init = pygame.display.set_mode(size)
 
 class button():
 
-    def __init__(self, color,  width, height):
+    def __init__(self, color, x , y , width, height):
         self.color = color
         self.width = width
         self.height = height
-        pygame.draw.rect(screen_init, self.color , [0, 0, self.width, self.height], 0)
+        self.x = x
+        self.y = y
+        self.shape(self.x,self.y)
 
-    def on_click(self, event):
+    
+    def shape(self, x, y):
+        pygame.draw.rect(screen_init, self.color , [x, y, x+self.width, y+self.height], 0)
+        
+        
+
+    def on_click(self, screen, event):
             if event.type == pygame.MOUSEBUTTONDOWN:
-                screen_init.fill('black')
-                pygame.draw.rect(screen_init, self.color , [100, 100, self.width, self.height], 0)
-                
+                screen.fill('black')
+                #screen.blit()
+ 
 
-but1 = button((0,0,255),100,150)
+    def set_text(self, text, color, screen, size):
+
+        font = pygame.font.Font(None, size)
+        ctext = font.render(text, 1, color)
+        screen.blit(ctext, ((2*self.x+self.width)/2, (2*self.y+self.height)/1.75))
+    
+
+
+
+
+
+
+but1 = button((0,0,255), 100, 100, 100, 100)
+but1.set_text('hello', (255,255,255), screen_init, 25)
+
 
 def main():
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            but1.on_click(event)
+            but1.on_click(screen_init , event)
 
         pygame.display.update()
         
